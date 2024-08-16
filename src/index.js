a33dfc756cbba9ca15903c7c10420ec57f5ef7cb
SALAH BKZ <117542403+salahbkz44@users.noreply.github.com>
2024-08-16 14:41:13 +0100

    adding initial content

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display a h1 heading.
+ReactDOM.render(
+  <div>
+    <h1>Heading</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
